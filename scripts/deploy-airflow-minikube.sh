f1922b97f830202cb39b03f87a5aad3fd6550d2c
André Cloete <ajcloete@gmail.com>
2019-07-14 11:59:58 +0200

    bug: Fix helm installer

--- scripts/deploy-airflow-minikube.sh
+++ scripts/deploy-airflow-minikube.sh
@@ -7,10 +7,10 @@ BASEDIR=$(dirname "$0")
 
 if [[ ! $(which helm) ]]; then
   echo Installing helm which is required for deploying to kubernetes
-  curl -LsS "https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get" -o get_helm.sh  
-  chmod +x get_helm.sh
-  sudo sh get_helm.sh
-  sudo rm get_helm.sh
+  curl -LO https://git.io/get_helm.sh
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  rm get_helm.sh
 fi
 
 # Apply the tiller service account on the minikube cluster
