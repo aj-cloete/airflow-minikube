8a0703526e9a91bf73856c0d67ccda11644cd850
Andre Cloete <andre@yoco.co.za>
2019-07-14 10:06:32 +0200

    Add add_remotes.sh to configure all the git remotes

---
+++ add_remotes.sh
@@ -0,0 +1,6 @@
+git remote add airflow https://github.com/apache/airflow \
+  && git fetch airflow v1-10-stable
+git remote add airflow-docker https://github.com/puckel/docker-airflow \
+  && git fetch airflow-docker 1.10.3
+git remote add brecht https://github.com/BrechtDeVlieger/airflow-kube-helm
+  && git fetch brecht
