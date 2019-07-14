878cbef89df8c474efe48c27921420d89b4d6242
André Cloete <ajcloete@gmail.com>
2019-07-14 11:46:10 +0200

    feat: Install helm if not installed

--- scripts/deploy-airflow-minikube.sh
+++ scripts/deploy-airflow-minikube.sh
@@ -5,6 +5,14 @@ BASEDIR=$(dirname "$0")
 # Start the minikube cluster
 /bin/bash $BASEDIR/start_minikube.sh;
 
+if [[ ! $(which helm) ]]; then
+  echo Installing helm which is required for deploying to kubernetes
+  curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+  chmod +x get_helm.sh
+  sudo sh get_helm.sh
+  sudo rm get_helm.sh
+fi
+
 # Apply the tiller service account on the minikube cluster
 kubectl apply -f $BASEDIR/../airflow/tiller.yaml
 helm init --service-account tiller --upgrade
