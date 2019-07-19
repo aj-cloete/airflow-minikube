c138256346fe0efdad08e40a98450bf787eb6ee1
André Cloete <ajcloete@gmail.com>
2019-07-19 14:01:41 +0200

    feat: Brew reinstall instead of install to get fewer issues

--- scripts/deploy-airflow-minikube.sh
+++ scripts/deploy-airflow-minikube.sh
@@ -12,7 +12,7 @@ BASEDIR=$(cd "$(dirname "$_MY_SCRIPT")" && pwd)
 if [[ ! $(which helm) ]]; then
   echo Installing helm which is required for deploying to kubernetes
   if [[ $(which brew) ]]; then
-    brew install kubernetes-helm; else
+    brew reinstall kubernetes-helm; else
     curl -LO https://git.io/get_helm.sh
     chmod 700 get_helm.sh
     ./get_helm.sh
