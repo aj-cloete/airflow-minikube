7488a6c985386009748eb86c7023f6b7fd3f6375
Andre Cloete <andre@yoco.co.za>
2019-08-15 11:16:34 +0200

    bug: Fix entrypoint to run scheduler as subprocess when Local or Sequential executors are used

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -57,7 +57,7 @@ fi
 case "$1" in
   webserver)
     airflow initdb
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
+    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
       # With the "Local" executor it should all run in one container.
       airflow scheduler &
     fi
